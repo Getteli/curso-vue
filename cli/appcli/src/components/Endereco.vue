4fc551090fc1aa5879d236811ef6514a330d6420
getteli <douglas.araujo.01@outlook.com>
2025-01-25 15:50:02 -0300

    Diretivas (logicas de programação): if, show, loop...

--- cli/appcli/src/components/Endereco.vue
+++ cli/appcli/src/components/Endereco.vue
@@ -6,7 +6,12 @@
                 endereco: {
                     rua: "Rua de tal",
                     numero: 12,
-                    cidade: "Rio de Janeiro"
+                    cidade: "Rio de Janeiro",
+                    cep: 20911070,
+                    // para o if
+                    is_apartamento: false,
+                    // para o show
+                    mostrar_cep: true,
                 }
             }
         }
@@ -15,4 +20,20 @@
 
 <template>
     <p>Endereço: {{ endereco.rua }}, n° {{ endereco.numero }}, {{ endereco.cidade }}</p>
+    <!-- diretivas -->
+    <div>
+        <p>Diretivas</p>
+        <p>If</p>
+            <!-- IF (v-if) -->
+            <!-- vai exibir o conteudo se a condição for true -->
+            <p v-if="endereco.is_apartamento">É um apartamento</p>
+            <!-- o else PRECISA estar logo em seguida, nao pode estar depois.  -->
+            <p v-else>É uma casa</p>
+        
+        <p>Show</p>
+            <!-- SHOW (um tipo de if simplificado) -->
+            <!-- ele vai exibir se algo for verdadeiro, PONTO. Não há um else nem nada do tipo, mas o show.-->
+            <p v-show="endereco.mostrar_cep">O CEP é {{ endereco.cep }}</p>
+    </div>
+    <!-- tem todas as outras logicas de programação, for, para loop, e etc.. Ver na DOC-->
 </template>
