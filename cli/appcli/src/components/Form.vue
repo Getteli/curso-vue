7ff8a2763758ca722c69a87ad4e781833cc5f344
getteli <douglas.araujo.01@outlook.com>
2025-01-25 17:11:23 -0300

    Eventos

--- cli/appcli/src/components/Form.vue
+++ cli/appcli/src/components/Form.vue
@@ -6,20 +6,36 @@
         name: "Form",
         data() {
             return {
-
+                inputValue: "",
             }
         },
         components: {
             InputText,
             ButtonSend
+        },
+        methods: {
+            enviar(e){
+                e.preventDefault();
+                const formData = new FormData(e.target);
+                const formProps = Object.fromEntries(formData);
+                console.log(formProps);
+
+                // Você pode acessar os valores individuais, como:
+                const nome = formProps.nome;
+                const email = formProps.email;
+                console.log(`Nome: ${nome}, Email: ${email}`);
+
+                alert("enviando");
+            }
         }
     }
 </script>
 
 <template>
-    <form action="" method="get">
-        <InputText :label="'Nome'" :nameInput="'nome'" :placeholderInput="'Digite seu nome'" :class="'form'" :id="'nomeInput'" :required="true" :disabled="false" />
-        <InputText :label="'Email'" :nameInput="'email'" :placeholderInput="'Digite seu email'" :class="'form'" :id="'emailInput'" :required="true" :disabled="false"/>
+    <!-- alem do click, outro evento do vue é o submit -->
+    <form action="#" method="get" @submit="enviar">
+        <InputText :label="'Nome'" :nameInput="'nome'" :placeholderInput="'Digite seu nome'" :class="'form'" :id="'nomeInput'" :required="true" :disabled="false" :maxLengthInput="50"/>
+        <InputText :label="'Email'" :nameInput="'email'" :placeholderInput="'Digite seu email'" :class="'form'" :id="'emailInput'" :required="true" :disabled="false" :maxLengthInput="50"/>
         <ButtonSend :label="'Enviar'" :id="'buttonId'" :class="'classButton'"/>
     </form>
 </template>
